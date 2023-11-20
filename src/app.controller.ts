78ca4bb517f482d3d35167c7190a243e5224ba26
NaufalK25 <naufalkateni2001@gmail.com>
2023-11-20 15:02:34 +0700

    add / path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,40 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
+  private baseUrl: string;
+  constructor(config: ConfigService) {
+    this.baseUrl = config.get('BASE_URL');
+  }
+
+  @ApiTags('welcome')
+  @Get()
+  getWelcome() {
+    return {
+      statusCode: 200,
+      message: 'Welcome to Bookmark API',
+      data: {
+        api: {
+          endpoint: `${this.baseUrl}/api`,
+          docs: `${this.baseUrl}/api/docs`,
+        },
+        graphql: `${this.baseUrl}/graphql`,
+      },
+    };
+  }
+
   @ApiTags('welcome')
   @Get('/api')
   getWelcomeApi() {
     return {
       statusCode: 200,
       message: 'Welcome to Bookmark API',
+      data: {
+        endpoint: `${this.baseUrl}/api`,
+        docs: `${this.baseUrl}/api/docs`,
+      },
     };
   }
 }
